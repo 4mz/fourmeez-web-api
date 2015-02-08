f8780eef89dcc45edfabaeeef96fd6eee1bd3b56
matbu <mat.bultel@gmail.com>
2015-02-08 19:13:58 +0100

    remove useless commented lines

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -49,26 +49,3 @@ fourmeezControllers.controller('SignUpCtrl', ['$scope','$http', '$location',
             }
     }
 ]);
-
-
-//  function($scope, $http){
-//        $http({
-//            method: 'POST',
-//            url: 'http://127.0.0.1:8080/signin/access_token/',
-//            data: "username="+$scope.userData.username+
-//                                "&password="+$scope.userData.password+
-//                                "&grant_type=password&client_id=b89d29260a645d3710c7",
-//            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-//                }).success(function(data, status, headers, config)
-//                {
-//                /*called for result & error because 200 status*/
-//                if (data.result){
-//                    //handle success here
-//                    } else if (data.error {
-//                    //handle error here
-//                    )}
-//                     }).error(function(data, status, headers, config){
-//                /*handle non 200 statuses*/
-//                    });
-//              }
-//]);
